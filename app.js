ae534020fe7699176d46e8ea8f8bf104caae776a
Matt Holmes <m4ttholmes@gmail.com>
2021-10-27 14:25:07 -0400

    Character Controller

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const Express = require('express');
 const app = Express();
-   
+
+const controllers = require("./controllers");
+
+
+app.use("/character", controllers.characterController);
+
 app.listen(3000, () => {
-    
+    console.log(`[Server]: App is listening on 3000.`);
 });
