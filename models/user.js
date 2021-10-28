258c7eef6342a47a91928d08e407f1cc8df772de
Riley Roush <riley.roush@gmail.com>
2021-10-28 09:12:13 -0400

    added user model and user controller with login and register functioning

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 const db = require("../db");
 
-const User = deb.define("user", {
+const User = db.define("user", {
     email: {
         type: DataTypes.STRING(100),
         allowNull: false,
@@ -9,6 +9,10 @@ const User = deb.define("user", {
     },
     password: {
         type: DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING(100),
         allowNull: false,
     },
     firstName: {
